923dc12c10c4c11390e67bdf0d97a5c2f70a9cca
Anass-ELASSRI <anass.elassri.04@gmail.com>
2025-08-29 21:32:03 +0100

    test new login with laravel session

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -40,7 +40,7 @@ class Kernel extends HttpKernel
 
         ],
         'api' => [
-            \App\Http\Middleware\InjectTokenFromCookie::class,
+            // \App\Http\Middleware\InjectTokenFromCookie::class,
             \App\Http\Middleware\CheckTokenExpiration::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
             \Illuminate\Routing\Middleware\ThrottleRequests::class . ':api',
